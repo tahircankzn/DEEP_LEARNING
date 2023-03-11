22119cee3d723e0889b682bd3effd2e6e7609f68
Tahir Can Kozan <93403102+tahircankzn@users.noreply.github.com>
2023-03-11 15:11:17 +0300

    Add files via upload

--- bias trying/passed1.py
+++ bias trying/passed1.py
@@ -5,13 +5,13 @@ import time
 w1 = random.randint(1,5)
 w2 = random.randint(1,5)
 bias = random.randint(1,5)
-
+learning_rate = 0.001 
 
 for i in range(10000):
     
 
-    number1 = 12
-    number2 = 12
+    number1 = 200
+    number2 = 200
 
     result = number1*number2
 
@@ -27,15 +27,16 @@ for i in range(10000):
 
     if  yakınlık == 0:  #if yakınlık < 0.01 and yakınlık > -0.01:   # if yakınlık == 0:
         #print(f"PASSED  {number1} * {number2} = {result} , nöron çıkışı : {nöron1_çıkış} , yakınlık : {yakınlık} ,cost : {yakınlık} , w1 : {w1} - w2 : {w2}")
-        print(f"yakınlık : {yakınlık} , w1 : {w1} - w2 : {w2}")
+        #print(f"yakınlık : {yakınlık} , w1 : {w1} - w2 : {w2}")
         dosya = open("w.txt","w")
         dosya.write(f"{w1} {w2}")
         dosya.close()
         break
     else:
-        w1 = w1 + derivative(number1) * 0.001 
-        w2 = w2 + derivative(number2) * 0.001
+        w1 = w1 + derivative(number1) * learning_rate
+        w2 = w2 + derivative(number2) * learning_rate
         bias = bias + ((nöron1_çıkış - result)*-1)
+        learning_rate = learning_rate - 0.001
         #print(f"nöron çıkışı : {nöron1_çıkış} , yakınlık : {yakınlık} , w1 : {w1} - w2 : {w2}")
     
 print(f"nöron çıkışı : {nöron1_çıkış} , yakınlık : {yakınlık} , w1 : {w1} - w2 : {w2} , bias : {bias}")
